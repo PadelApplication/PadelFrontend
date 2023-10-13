580969f3da326d30430983f561f7d8eb67a78e5b
SteijnPloegmakers <sploegmakers@inforit.nl>
2023-10-13 12:10:06 +0200

    Made it so when any URL which is not in router is visited, it automatically redirects to events

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Events from "./Pages/Events/Events";
 import Navbar from "./Components/Navigation/Navbar";
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import CreateEvent from "./Pages/Events/CreateEvent";
 
 function App() {
@@ -10,6 +10,7 @@ function App() {
       <div className="App-header">
           <Navbar/>
           <Routes>
+              <Route path="*" element={<Navigate to="/events" />} />
               <Route exact path="/events" element={<Events/>} />
               <Route exact path="/create" element={<CreateEvent/>} />
           </Routes>
